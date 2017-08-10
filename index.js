c5a076f296bb4a761b2108dc8f301d55c9d4e767
bsarvaiya <bsarvaiya@officebrain.com>
2017-08-10 23:20:37 +0530

    get methods added

--- index.js
+++ index.js
@@ -119,3 +119,125 @@ module.exports.indexLast = function (a, q){
     return null;
   }
 }
+
+module.exports.getAll = function (a, q){
+  var ix = [];
+  if(a instanceof Array){
+    for (var i in a){
+      var b = true;
+      for(k in q){
+        if(a[i][k]!=q[k]){
+          b = false;
+          break;
+        }
+      }
+      if(b){
+        ix.push(a[i]);
+      }
+    }
+    return ix;
+  }else{
+    console.log('\x1b[31m%s\x1b[0m','... object is not of type array');
+    return null;
+  }
+}
+
+module.exports.getFirst = function (a, q){
+  var ix = null;
+  if(a instanceof Array){
+    for (var i in a){
+      var b = true;
+      for(k in q){
+        if(a[i][k]!=q[k]){
+          b = false;
+          break;
+        }
+      }
+      if(b){
+        ix = a[i];
+        break;
+      }
+    }
+    return ix;
+  }else{
+    console.log('\x1b[31m%s\x1b[0m','... object is not of type array');
+    return null;
+  }
+}
+
+module.exports.getFirstN = function (a, q, n){
+  var ix = [];
+  var f = 0;
+  if(a instanceof Array){
+    for (var i in a){
+      var b = true;
+      for(k in q){
+        if(a[i][k]!=q[k]){
+          b = false;
+          break;
+        }
+      }
+      if(b){
+        ix.push(a[i]);
+        f++;
+        if(f == n){
+          break;
+        }
+      }
+    }
+    return ix;
+  }else{
+    console.log('\x1b[31m%s\x1b[0m','... object is not of type array');
+    return null;
+  }
+}
+
+module.exports.getLastN = function (a, q, n){
+  var ix = [];
+  var f = 0;
+  if(a instanceof Array){
+    for (var i = a.length-1; i>=0; i--){
+      var b = true;
+      for(k in q){
+        if(a[i][k]!=q[k]){
+          b = false;
+          break;
+        }
+      }
+      if(b){
+        ix.push(a[i]);
+        f++;
+        if(f == n){
+          break;
+        }
+      }
+    }
+    return ix.reverse();
+  }else{
+    console.log('\x1b[31m%s\x1b[0m','... object is not of type array');
+    return null;
+  }
+}
+
+module.exports.getLast = function (a, q){
+  var ix = null;
+  if(a instanceof Array){
+    for (var i = a.length-1; i>=0; i--){
+      var b = true;
+      for(k in q){
+        if(a[i][k]!=q[k]){
+          b = false;
+          break;
+        }
+      }
+      if(b){
+        ix = a[i];
+        break;
+      }
+    }
+    return ix;
+  }else{
+    console.log('\x1b[31m%s\x1b[0m','... object is not of type array');
+    return null;
+  }
+}
