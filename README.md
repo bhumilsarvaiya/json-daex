297df1d3b1c0a43c52f2adc534c1b7a250e31190
bsarvaiya <bsarvaiya@officebrain.com>
2017-08-31 09:20:58 +0530

    rename, update and remove methods added

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 # Installation
 ```bash
-npm install json-daex -s
+npm install json-daex
 ```
 
 # About
 This project is currently in very early stage. Please feel free to share any idea to make this package better.
 
-Currently, you will be able to get index of objects or objects from a JSON array based on the query passed.
-
 # Documentation
-+ json-daex [wiki](https://github.com/bhumilsarvaiya/json-daex/wiki)
++ [click here](https://github.com/bhumilsarvaiya/json-daex/wiki) to see documentation
 
-# Currently supported functions and query:
+# Usage Example:
 
 ```javascript
 var daex = require('json-daex')
@@ -43,73 +41,10 @@ var obj = [
   }
 ]
 
-console.log(daex.indexAll(obj,{'type':'array'}))
+var result = daex.indexAll(obj,{'type':'array'})
+console.log(result)
 //get index of all objects with type: array
 // Output: [ 0, 2, 3 ]
-
-console.log(daex.indexAll(obj,{'type':'array','age':20}))
-//get index of all objects with type: array and age: 20
-// Output: [ 0 , 3 ]
-
-console.log(daex.indexFirst(obj,{'type':'array'}))
-//get index of first object with type: array
-// Output: 0
-
-console.log(daex.indexLast(obj,{'type':'array'}))
-//get index of last object with type: array
-// Output: 3
-
-console.log(daex.indexFirstN(obj,{'type':'array'},2))
-//get index of first 2 objects with type: array
-// Output: [ 0, 2 ]
-
-console.log(daex.indexLastN(obj,{'type':'array'},2))
-//get index of last 2 objects with type: array
-// Output: [ 2, 3 ]
-
-console.log(daex.getAll(obj,{'type':'array'}))
-//get all objects with type: array
-// Output: [ { name: 'abc', type: 'array', age: 20 },
-//           { name: 'xyz', type: 'array', age: 22 },
-//           { name: 'ghi', type: 'array', age: 20 } ]
-
-console.log(daex.getAll(obj,{'type':'array','age':20}))
-//get all objects with type: array and age: 20
-// Output: [ { name: 'abc', type: 'array', age: 20 },
-//           { name: 'ghi', type: 'array', age: 20 } ]
-
-console.log(daex.getFirst(obj,{'type':'array'}))
-//get first object with type: array
-// Output: { name: 'abc', type: 'array', age: 20 }
-
-console.log(daex.getLast(obj,{'type':'array'}))
-//get last object with type: array
-// Output: { name: 'ghi', type: 'array', age: 20 }
-
-console.log(daex.getFirstN(obj,{'type':'array'},2))
-//get first 2 objects with type: array
-// Output: [ { name: 'abc', type: 'array', age: 20 },
-//           { name: 'xyz', type: 'array', age: 22 } ]
-
-console.log(daex.getLastN(obj,{'type':'array'},2))
-//get last 2 objects with type: array
-// Output: [ { name: 'xyz', type: 'array', age: 22 },
-//           { name: 'ghi', type: 'array', age: 20 } ]
-
-console.log(daex.count(obj,{'type':'array'}))
-//count objects with type: array
-// Output: 3
-
-console.log(daex.inArray(obj,{'type':'array'}))
-//check if there exists any object with type: array
-// Output: true
-
-console.log(daex.inObject(obj[1],{'type':'array'}))
-//check if there exists type: array (key-value pair) in object
-// Output: true
-
-console.log(daex.except(obj,{'type':'array'}))
-//returns all objects which do not have type: array
-// Output: [ { name: 'pqr', type: 'string', age: 20 },
-//           { name: 'mno', type: 'number', age: 20 } ]
 ```
+
++ To see other available methods, refer [documentation](https://github.com/bhumilsarvaiya/json-daex/wiki)
--- index.js
+++ index.js
@@ -1,314 +1,327 @@
-function notArrayError(){
+function notArrayError () {
   throw new Error('object is not of type array')
 }
 
-function notObjectError(){
+function notObjectError () {
   throw new Error('object type error')
 }
 
-module.exports.indexAll = function (a, q){
+module.exports.indexAll = function (a, q) {
   var ix = []
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
-        ix.push(parseInt(i))
-      }
-    }
+  if (a instanceof Array) {
+    for (var i in a) if (q == undefined || this.inObject(a[i], q)) ix.push(parseInt(i))
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.indexFirst = function (a, q){
+module.exports.indexFirst = function (a, q) {
   var ix = null
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix = parseInt(i)
         break
       }
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.indexFirstN = function (a, q, n){
+module.exports.indexFirstN = function (a, n, q) {
   var ix = []
   var f = 0
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix.push(parseInt(i))
         f++
-        if(f == n){
-          break
-        }
+        if (f == n) break
       }
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.indexLastN = function (a, q, n){
+module.exports.indexLastN = function (a, n, q) {
   var ix = []
   var f = 0
-  if(a instanceof Array){
-    for (var i = a.length-1; i>=0; i--){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i = a.length-1; i>=0; i--) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix.push(parseInt(i))
         f++
-        if(f == n){
-          break
-        }
+        if (f == n) break
       }
     }
     return ix.reverse()
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.indexLast = function (a, q){
+module.exports.indexLast = function (a, q) {
   var ix = null
-  if(a instanceof Array){
-    for (var i = a.length-1; i>=0; i--){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i = a.length-1; i>=0; i--) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix = parseInt(i)
         break
       }
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.getAll = function (a, q){
+module.exports.getAll = function (a, q) {
   var ix = []
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
-        ix.push(a[i])
-      }
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) ix.push(a[i])
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.getFirst = function (a, q){
+module.exports.getFirst = function (a, q) {
   var ix = null
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix = a[i]
         break
       }
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.getFirstN = function (a, q, n){
+module.exports.getFirstN = function (a, n, q) {
   var ix = []
   var f = 0
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix.push(a[i])
         f++
-        if(f == n){
-          break
-        }
+        if (f == n) break
       }
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.getLastN = function (a, q, n){
+module.exports.getLastN = function (a, n, q) {
   var ix = []
   var f = 0
-  if(a instanceof Array){
-    for (var i = a.length-1; i>=0; i--){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i = a.length-1; i>=0; i--) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix.push(a[i])
         f++
-        if(f == n){
-          break
-        }
+        if (f == n) break
       }
     }
     return ix.reverse()
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.getLast = function (a, q){
+module.exports.getLast = function (a, q) {
   var ix = null
-  if(a instanceof Array){
-    for (var i = a.length-1; i>=0; i--){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
+  if (a instanceof Array) {
+    for (var i = a.length-1; i>=0; i--) {
+      if (q == undefined || this.inObject(a[i], q)) {
         ix = a[i]
         break
       }
     }
     return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.count = function (a, q){
+module.exports.count = function (a, q) {
   var counter = 0
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
-        counter++
-      }
-    }
+  if (a instanceof Array) {
+    for (var i in a) if (q == undefined || this.inObject(a[i], q)) counter++
     return counter
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.inArray = function (a, q){
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          break
-        }
-      }
-      if(b){
-        return true
-      }
-    }
+module.exports.inArray = function (a, q) {
+  if (a instanceof Array) {
+    for (var i in a) if (q == undefined || this.inObject(a[i], q)) return true
     return false
-  }else{
-    notArrayError()
   }
+  else notArrayError()
 }
 
-module.exports.inObject = function (a, q){
-  if(typeof a == 'object' && !(a instanceof Array)){
-    for(k in q){
-      if(a[k]!=q[k]){
-        return false
-      }
-    }
+module.exports.inObject = function (a, q) {
+  if (typeof a == 'object' && !(a instanceof Array)) {
+    for (k in q) if (a[k] != q[k]) return false
     return true
-  }else{
-    notObjectError()
   }
+  else notObjectError()
 }
 
-module.exports.except = function (a, q){
+module.exports.except = function (a, q) {
   var ix = []
-  if(a instanceof Array){
-    for (var i in a){
-      var b = true
-      for(k in q){
-        if(a[i][k]!=q[k]){
-          b = false
-          ix.push(a[i])
-          break
-        }
+  if (a instanceof Array) {
+    for (var i in a) if (q == undefined || !(this.inObject(a[i], q))) ix.push(a[i])
+    return ix
+  }
+  else notArrayError()
+}
+
+module.exports.update = function (a, v, q) {
+  if (typeof a == 'object' && !(a instanceof Array)) {
+    if (q == undefined || this.inObject(a, q)) for (k in v) a[k] = v[k]
+  }
+  else notObjectError()
+}
+
+module.exports.updateAll = function (a, v, q) {
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) for (k in v) a[i][k] = v[k]
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.updateFirst = function (a, v, q) {
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) {
+        for(k in v) a[i][k] = v[k]
+        break
       }
     }
-    return ix
-  }else{
-    notArrayError()
   }
+  else notArrayError()
+}
+
+module.exports.updateFirstN = function (a, v, n, q) {
+  var f = 0
+  if (a instanceof Array) {
+    for (var i in a) {
+      if (q == undefined || this.inObject(a[i], q)) {
+        f++
+        for (k in v) a[i][k] = v[k]
+        if (f == n) break
+      }
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.updateLast = function (a, v, q) {
+  if (a instanceof Array) {
+    for (var i = a.length-1; i>=0; i--){
+      if (q == undefined || this.inObject(a[i], q)) {
+        for(k in v) a[i][k] = v[k]
+        break
+      }
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.updateLastN = function (a, v, n, q) {
+  var f = 0
+  if (a instanceof Array) {
+    for (var i = a.length-1; i>=0; i--) {
+      if (q == undefined || this.inObject(a[i], q)) {
+        f++
+        for (k in v) a[i][k] = v[k]
+        if (f == n) break
+      }
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.removeAll = function (a, q) {
+  if (a instanceof Array) {
+    if (q == undefined) a.splice(0,a.length)
+    else {
+      var t = this.except(a, q)
+      a.splice(0, a.length)
+      for(var i in t) a.push(t[i])
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.removeFirst = function (a, q) {
+  if (a instanceof Array) {
+    if (q == undefined) a.splice(0, 1)
+    else a.splice(this.indexFirst(a, q), 1)
+  }
+  else notArrayError()
+}
+
+module.exports.removeFirstN = function (a, n, q) {
+  if (a instanceof Array) {
+    n = (n > a.length) ? a.length : n
+    if (q == undefined) a.splice(0, n)
+    else {
+      var t = this.indexFirstN(a, q, n).reverse()
+      for (var i in t) a.splice(t[i], 1)
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.removeLast = function (a, q) {
+  if (a instanceof Array) {
+    if (q == undefined) a.splice(a.length - 1, 1)
+    else a.splice(this.indexLast(a, q), 1)
+  }
+  else notArrayError()
+}
+
+module.exports.removeLastN = function (a, n, q) {
+  if (a instanceof Array) {
+    n = (n > a.length) ? a.length : n
+    if (q == undefined) a.splice(a.length - n, n)
+    else {
+      var t = this.indexLastN(a, q, n).reverse()
+      for (var i in t) a.splice(t[i], 1)
+    }
+  }
+  else notArrayError()
+}
+
+module.exports.removeFields = function (a, f, q) {
+  if (a instanceof Array && f instanceof Array) {
+    for (var i in a) if (q == undefined || this.inObject(a[i], q)) for (var k in f) delete a[i][f[k]]
+  }
+  else if (typeof a == 'object' && f instanceof Array) {
+    if (q == undefined || this.inObject(a, q)) for (var k in f) delete a[f[k]]
+  }
+  else notArrayError()
+}
+
+module.exports.renameFields = function (a, f, q) {
+  if (a instanceof Array) {
+    for (var i in a) if (q == undefined || this.inObject(a[i], q)) for (k in f) {
+      a[i][f[k]] = a[i][k] ? a[i][k] : null
+      delete a[i][k]
+    }
+  }
+  else if (typeof a == 'object') {
+    if (q == undefined || this.inObject(a, q)) for (k in f) {
+      a[f[k]] = a[k] ? a[k] : null
+      delete a[k]
+    }
+  }
+  else notArrayError()
 }
